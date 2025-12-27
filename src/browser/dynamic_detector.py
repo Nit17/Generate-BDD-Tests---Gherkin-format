e932bcb67d37a3e5efb879d00ded684e6b831e83
Nit17 <nithinbm17@gmail.com>
2025-12-27 13:38:10 +0530

    new

--- src/browser/dynamic_detector.py
+++ src/browser/dynamic_detector.py
@@ -9,6 +9,7 @@ This module provides fully autonomous element detection by:
 4. Analyzing DOM structure and relationships
 """
 
+
 import asyncio
 import logging
 from typing import List, Dict, Any, Optional, Set, Tuple
